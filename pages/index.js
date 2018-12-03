eaa2b203d727e987507d371f2c4d44185e720782
Maximillian Silva <maxsilvauk@gmail.com>
2018-12-03 03:27:31 +0000

    Change of layout

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,13 @@ import factory from "../ethereum/factory.js";
 import Link from "next/link";
 import Head from "../components/head";
 import Nav from "../components/nav";
-import { Card, Button } from "semantic-ui-react";
+import {
+  Container,
+  Card,
+  Button,
+  Header,
+  Menu,
+} from 'semantic-ui-react'
 
 class CampaignIndex extends Component {
   // Next handles this. If they dont have JS or on mobile
@@ -28,66 +34,23 @@ class CampaignIndex extends Component {
   render() {
     return (
       <div>
-        <link
-          rel="stylesheet"
-          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css"
-        />
-        <Head title="Home" />
-        <Nav />
-        <div className="hero">
-          <h1 className="title">Campaigns!</h1>
-          <div className="description">
-          {this.renderCampaigns()}
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.0/dist/semantic.min.css" />
+        <Menu fixed='top' inverted>
+          <Container>
+            <Menu.Item as='a' header>
+              Ethereum KickStarter Campaign
+            </Menu.Item>
+            <Menu.Item as='a'>List Campaigns</Menu.Item>
+            <Menu.Item as='a'>Create New</Menu.Item>
+          </Container>
+        </Menu>
+        <Container text style={{ marginTop: '7em' }}>
+          <Header as='h1'>Campaigns</Header>
           <Button content="Create Campaign" icon="add circle" primary />
-          </div>
-        </div>
-  
-        <style jsx>{`
-          .hero {
-            width: 100%;
-            color: #333;
-          }
-          .title {
-            margin: 0;
-            width: 100%;
-            padding-top: 80px;
-            line-height: 1.15;
-            font-size: 48px;
-          }
-          .title,
-          .description {
-            text-align: center;
-          }
-          .row {
-            max-width: 880px;
-            margin: 80px auto 40px;
-            display: flex;
-            flex-direction: row;
-            justify-content: space-around;
-          }
-          .card {
-            padding: 18px 18px 24px;
-            width: 220px;
-            text-align: left;
-            text-decoration: none;
-            color: #434343;
-            border: 1px solid #9b9b9b;
-          }
-          .card:hover {
-            border-color: #067df7;
-          }
-          .card h3 {
-            margin: 0;
-            color: #067df7;
-            font-size: 18px;
-          }
-          .card p {
-            margin: 0;
-            padding: 12px 0 0;
-            font-size: 13px;
-            color: #333;
-          }
-        `}</style>
+          <div>{this.renderCampaigns()}</div>
+          <p>This is a basic fixed menu template using fixed size containers.</p>
+          <p>A text container is used for the main container, which is useful for single column layouts.</p>
+        </Container>
       </div>
     );
   }
